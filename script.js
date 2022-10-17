c01bd3aa1f752e009124e21165d3b6f2891c0755
D. Akelar <108392983+akelarka@users.noreply.github.com>
2022-10-17 22:58:02 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,21 +5,6 @@ fetch('http://api.openweathermap.org/data/2.5/weather?id=709493&appid=1d2d9d201c
         document.querySelector('.package-name').textContent = data.name;
         document.querySelector('.price').innerHTML = Math.round(data.main.temp - 273) + '&deg;';
         document.querySelector('.disclaimer').textContent = data.weather[0]['description'];
-        //https://openweathermap.org/img/wn/02d@2x.png
         document.querySelector('.features li').innerHTML = `<img src="https://openweathermap.org/img/wn/${data.weather[0]['icon']}@2x.png">`;
     })
-    .catch(function () {
-        // catch any errors
-    });
-
-// {
-//         "id": 709493,
-//         "name": "Dubrovytsya",
-//         "state": "",
-//         "country": "UA",
-//         "coord": {
-//             "lon": 26.565029,
-//             "lat": 51.574379
-//         }
-//     },
 
